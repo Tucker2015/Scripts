1ce642d58f76a6b0dfd386fd248aeb9fdfbce155
Kevin Tucker <mail@kevtucker.com>
2018-03-30 12:41:24 +0100

    Initial Commit

---
+++ nginx_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+docker run -d -p 80:80 -p 443:443 \
+    --name nginx-proxy \
+    -v /etc/docker/nginx:/etc/nginx/certs:ro \
+    -v /etc/nginx/vhost.d \
+    -v /usr/share/nginx/html \
+    -v /var/run/docker.sock:/tmp/docker.sock:ro \
+    --label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy \
+    jwilder/nginx-proxy
+    
+docker run -d \
+    -v /etc/docker/nginx:/etc/nginx/certs:rw \
+    -v /var/run/docker.sock:/var/run/docker.sock:ro \
+    --volumes-from nginx-proxy \
+    jrcs/letsencrypt-nginx-proxy-companion
