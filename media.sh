8abdb137f03d80010b54f6f8e4ed9e5e5c850b9e
Kevin Tucker <mail@kevtucker.com>
2018-03-30 13:25:29 +0100

    Add Radarr and Nzbget

--- media.sh
+++ media.sh
@@ -15,4 +15,25 @@ docker run -d \
     -e "LETSENCRYPT_EMAIL=mail@kevtucker.com" \
     linuxserver/sonarr
     
-    
+docker run -d \
+  --name=radarr \
+    -v /etc/docker/radarr:/config \
+    -v /home/kevin/downloads:/downloads \
+    -v /mnt/media:/movies \
+    -e PGID=1000 -e PUID=1000  \
+    -p 7878:7878 \
+    -e "VIRTUAL_HOST=radarr.kevtucker.com" \
+    -e "LETSENCRYPT_HOST=radarr.kevtucker.com" \
+    -e "LETSENCRYPT_EMAIL=mail@kevtucker.com" \
+    linuxserver/radarr
+  
+docker run -d \
+    --name nzbget \
+    -p 6789:6789 \
+    -e PUID=1000 -e PGID=1000 \
+    -v /etc/docker/nzbget:/config \
+    -v /home/kevin/downloads:/downloads \
+    -e "VIRTUAL_HOST=nzb.kevtucker.com" \
+    -e "LETSENCRYPT_HOST=nzb.kevtucker.com" \
+    -e "LETSENCRYPT_EMAIL=mail@kevtucker.com" \
+    linuxserver/nzbget
