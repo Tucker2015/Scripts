291609834007374be40748e19034fbb9a9db19f5
Kevin Tucker <mail@kevtucker.com>
2018-03-31 10:27:47 +0100

    Add Restart Always and Portainer

--- media.sh
+++ media.sh
@@ -5,6 +5,7 @@
 
 docker run -d \
     --name sonarr \
+    --restart always \
     -p 8989:8989 \
     -e PUID=1000 -e PGID=1000 \
     -v /etc/docker/sonarr:/config \
@@ -19,6 +20,7 @@ docker run -d \
     
 docker run -d \
   --name=radarr \
+  --restart always \
     -v /etc/docker/radarr:/config \
     -v /home/kevin/downloads:/downloads \
     -v /mnt/media/movies:/movies \
@@ -33,6 +35,7 @@ docker run -d \
   
 docker run -d \
     --name nzbget \
+    --restart always \
     -p 6789:6789 \
     -e PUID=1000 -e PGID=1000 \
     -v /etc/docker/nzbget:/config \
@@ -42,4 +45,30 @@ docker run -d \
     -e "LETSENCRYPT_EMAIL=mail@kevtucker.com" \
     linuxserver/nzbget
     
+## Install Portainer
+
+docker run -d \
+	--name portainer \
+	-p 9000:9000 \
+	--restart always \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v /opt/portainer:/data \
+	-e "VIRTUAL_HOST=docker.kevtucker.com" \
+    -e "LETSENCRYPT_HOST=docker.kevtucker.com" \
+    -e "LETSENCRYPT_EMAIL=mail@kevtucker.com" \
+	portainer/portainer
+	
 ## Install Deluge
+
+docker run -d \
+  --name deluge \
+  --net=host \
+  --restart always \
+  -e PUID=1000 -e PGID=1000 \
+  -e "VIRTUAL_HOST=deluge.kevtucker.com" \
+  -e "LETSENCRYPT_HOST=deluge.kevtucker.com" \
+  -e "LETSENCRYPT_EMAIL=mail@kevtucker.com" \
+  -e "VIRTUAL_PORT=8112"
+  -v ~/downloads:/downloads \
+  -v /etc/docker/deluge:/config \
+  linuxserver/deluge
